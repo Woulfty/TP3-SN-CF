39cb54252103afa99f578c385f954f8d11ab46be
Woulfty <70692837+Woulfty@users.noreply.github.com>
2021-11-29 11:49:29 +0100

    correction bug et ajout du socket pour formulaire

--- JS/socket.js
+++ JS/socket.js
@@ -1,32 +1,18 @@
 var socket = new WebSocket("ws://192.168.65.144:1234");
 
 //DIV
-var inscription = document.getElementById('inscription');
 var connexion = document.getElementById('connexion');
 
 //Champ
 var inputLogin_Connexion = document.getElementById("inputLogin_Connexion");
 var inputPassword_Connexion = document.getElementById("inputPassword_Connexion");
-var inputLogin_Inscription = document.getElementById("inputLogin_Inscription");
-var inputPassword_Inscription = document.getElementById("inputPassword_Inscription");
-var inputMessage = document.getElementById("inputMessage");
 
 //Bouton
 var inputConnect_Connexion = document.getElementById('inputConnect_Connexion');
-var inputRegister_Connexion = document.getElementById('inputRegister_Connexion');
-var inputConnect_Inscription = document.getElementById('inputConnect_Inscription');
-var inputRegister_Inscription = document.getElementById('inputRegister_Inscription');
+
 var inputSend = document.getElementById('inputSend');
 
-inputRegister_Inscription.addEventListener('click', function(){
-    var login = inputLogin_Inscription.value;
-    var mdp = inputPassword_Inscription.value;
-
-    data = "Salt"+login+";"+mdp;
-
-    socket.send(data);
-});
-
+//Connexion
 inputConnect_Connexion.addEventListener('click', function(){
     var login = inputLogin_Connexion.value;
     var mdp = inputPassword_Connexion.value;
@@ -34,16 +20,6 @@ inputConnect_Connexion.addEventListener('click', function(){
     data = "Auth"+login+";"+mdp;
 
     socket.send(data);
-});
-
-inputSend.addEventListener('click', function(){
-    var message = inputMessage.value
-
-    data ="Bdd"+message;
-    
-    socket.send(data);
-
-    inputMessage.value = "";
 });
 
 // Lorsque la connexion est établie.
